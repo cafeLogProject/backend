16518a39a482661f85642abee61b4cf3f8c95e78
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2024-12-13 15:51:21 +0900

    first-commit

---
+++ src/main/java/cafeLogProject/cafeLog/CafeLogApplication.java
@@ -0,0 +1,13 @@
+package cafeLogProject.cafeLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafeLogProject/cafeLog/CafeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package cafeLogProject.cafeLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
