0546ace310237601c1fb04414fe13fd71e51bea2
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2025-02-11 18:08:40 +0900

    fix: TestController 경로 수정

--- src/main/java/cafeLogProject/cafeLog/api/TestController.java
+++ src/main/java/cafeLogProject/cafeLog/api/TestController.java
@@ -11,22 +11,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TestController {
 
-    @GetMapping("/test/http/500")
+    @GetMapping("/api/test/http/500")
     public ResponseEntity<?> testGet500Error() {
         throw new UnexpectedServerException("에러 테스트입니다", ErrorCode.UNEXPECTED_ERROR);
     }
 
-    @PostMapping("/test/http/500")
+    @PostMapping("/api/test/http/500")
     public ResponseEntity<?> testPost500Error() {
         throw new UnexpectedServerException("에러 테스트입니다", ErrorCode.UNEXPECTED_ERROR);
     }
 
-    @GetMapping("/test/http/204")
+    @GetMapping("/api/test/http/204")
     public ResponseEntity<?> testGet204() {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/test/http/204")
+    @PostMapping("/api/test/http/204")
     public ResponseEntity<?> testPost204() {
         return ResponseEntity.noContent().build();
     }
