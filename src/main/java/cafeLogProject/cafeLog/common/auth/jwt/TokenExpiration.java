8d4f72e4c285968003c58d1198845f70e0355588
kimbc715 <kimbc715@gmail.com>
2025-03-02 19:16:39 +0900

    refactor: Access Token Expiration Ms

--- src/main/java/cafeLogProject/cafeLog/common/auth/jwt/TokenExpiration.java
+++ src/main/java/cafeLogProject/cafeLog/common/auth/jwt/TokenExpiration.java
@@ -2,6 +2,6 @@ package cafeLogProject.cafeLog.common.auth.jwt;
 
 public class TokenExpiration {
 
-    public static final long ACCESS_TOKEN_EXPIRATION = 30 * 1000L; // 30분
+    public static final long ACCESS_TOKEN_EXPIRATION = 30 * 60 * 1000L; // 30분
     public static final long REFRESH_TOKEN_EXPIRATION = 14 * 24 * 60 * 60 * 1000L; // 14일
 }
